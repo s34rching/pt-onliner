bf5301af3070847947702dd53a6eb6401fa4d19a
s34rching <ivan.katsenya@gmail.com>
2019-09-21 23:10:00 +0300

    Add lint rukes

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,21 +16,26 @@ module.exports = {
 		'ecmaVersion': 2018
 	},
 	'rules': {
-		'indent': [
-			'error',
-			'tab'
+		'array-bracket-spacing': ['error', 'always',
+			{
+				"arraysInArrays": false, // [[ 1, 2 ], 2, [ 3, 4 ]] instead of [ [ 1, 2 ], 2, [ 3, 4 ] ]
+				"objectsInArrays": false, // [{ 'foo': 'bar' }] instead of [ { 'foo': 'bar' } ]
+				"singleValue": false // [0] instead of [ 0 ]
+			}
 		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
+		'indent': [ 'error', 'tab' ],
+		'linebreak-style': [ 'error', 'windows'	],
+		'quotes': [	'error', 'single' ],
+		'semi': [ 'error', 'always'	],
+		'no-console': ['error'],
+		'no-duplicate-imports': ['error'],
+		'no-multiple-empty-lines': ['error'],
+		'no-var': ['error'],
+		'no-trailing-spaces': ['error'],
+		'object-curly-spacing': [ "error","always" ],
+		'sort-imports': ['error'],
+		'space-in-parens': [ 'error', 'never' ],
+		'arrow-parens': [ 'error', 'as-needed' ],
+		'eol-last': ['error', 'always']
 	}
 };
