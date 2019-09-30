2c2a6db881fa7e38b1443032537b5d0234386488
s34rching <ivan.katsenya@gmail.com>
2019-09-30 22:04:10 +0300

    Add launch task

--- spec.js
+++ spec.js
@@ -1,7 +1,7 @@
 describe('Protractor Demo App', function() {
-    it('should have a title', function() {
-        browser.get('http://juliemr.github.io/protractor-demo/');
+	it('should have a title', function() {
+		browser.get('http://juliemr.github.io/protractor-demo/');
 
-        expect(browser.getTitle()).toEqual('Super Calculator');
-    });
+		expect(browser.getTitle()).toEqual('Super Calculator');
+	});
 });
