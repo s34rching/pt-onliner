32dbae5b0e180891c7b93373fcd3e24dc12e1c07
s34rching <ivan.katsenya@gmail.com>
2019-10-07 23:02:31 +0300

    Remove linebreak style from rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,6 @@ module.exports = {
 			}
 		],
 		'indent': [ 'error', 'tab' ],
-		'linebreak-style': [ 'error', 'windows'	],
 		'quotes': [	'error', 'double' ],
 		'semi': [ 'error', 'always'	],
 		'no-console': ['error'],
