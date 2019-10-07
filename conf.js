c6b8f04205175c5c2d6ee9494583e88052e9d3ff
s34rching <ivan.katsenya@gmail.com>
2019-10-07 22:41:04 +0300

    Revert "commit fb1a938c3a13c04aa4d5c6d7f03c19e01d3841da"
    This reverts commit 270bc58eac210ed3782916bbcb8ce430e64535c8.

--- conf.js
+++ conf.js
@@ -1,6 +1,5 @@
 exports.config = {
-	baseUrl: "https://www.onliner.by",
 	framework: "jasmine",
 	seleniumAddress: "http://localhost:4444/wd/hub",
-	specs: ["specs/products-search/*.js"]
+	specs: ["spec.js"]
 };
--- specs/products-search/search-bar.spec.js
+++
@@ -1,8 +0,0 @@
-describe("Onliner.by - Product search using search bar", () => {
-	describe("Positive cases", () => {
-		it("Should show results if product exists", () => {
-			browser.get("/");
-			element(by.name("query")).click();
-		});
-	});
-});
