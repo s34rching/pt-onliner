a9107dea48ef5979af4d88d7cac840b30ee8a7a4
s34rching <ivan.katsenya@gmail.com>
2019-10-07 23:58:23 +0300

    Add scenarios to describe

--- specs/search.spec.js
+++ specs/search.spec.js
@@ -1,5 +1,17 @@
 describe("Onliner.by Products Search", () => {
-	it("", () => {
+	it("should search product by its full product name", () => {
+
+	})
+
+	it("should search products by category name", () => {
+
+	})
+
+	it("should allow to abort search", () => {
+
+	})
+
+	it("should NOT find non-existent product", () => {
 
 	})
 })
