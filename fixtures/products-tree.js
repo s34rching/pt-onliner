5bdcc4b752bed71d813c776d3365577c8ddb6f5c
s34rching <ivan.katsenya@gmail.com>
2019-10-08 23:27:07 +0300

    Add products fixture file

---
+++ fixtures/products-tree.js
@@ -0,0 +1,31 @@
+module.exports = {
+	classifiers: {
+		electronics: {
+			id: 1
+		},
+		computers: {
+			id: 2
+		},
+		houseAppliances: {
+			id: 3
+		},
+		constructionAndRepair: {
+			id: 4
+		},
+		houseAndGarden: {
+			id: 5
+		},
+		carsAndCycles: {
+			id: 6
+		},
+		beautyAndSport: {
+			id: 7
+		},
+		childrenAndMamas: {
+			id: 8
+		},
+		workAndOffice: {
+			id: 9
+		}
+	}
+}
