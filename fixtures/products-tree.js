105b92c034e6fbbe09cba43fad6b51967937c05c
s34rching <ivan.katsenya@gmail.com>
2019-10-09 22:01:54 +0300

    Add products fixture draft

---
+++ fixtures/products-tree.js
@@ -0,0 +1,29 @@
+module.exports = {
+	electronics: {
+		id: 1
+	},
+	computers: {
+		id: 2
+	},
+	houseAppliances: {
+		id: 3
+	},
+	constructionAndRepair: {
+		id: 4
+	},
+	houseAndGarden: {
+		id: 5
+	},
+	carsAndCycles: {
+		id: 6
+	},
+	beautyAndSport: {
+		id: 7
+	},
+	childrenAndMamas: {
+		id: 8
+	},
+	workAndOffice: {
+		id: 9
+	}
+}
