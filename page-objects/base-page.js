1e770fb5d826298adec966c459f380dc57fe940d
ivankatsenya <ivan.katsenya@freshly.com>
2019-10-09 12:40:16 +0300

    Move page objects methods outside of constructor

--- page-objects/base-page.js
+++ page-objects/base-page.js
@@ -1,25 +1,24 @@
 class BasePage {
-	constructor() {
-		this.searchBar = element(by.css("input.fast-search__input"))
+	constructor() {}
+	// searchBar = element(by.css("input.fast-search__input"))
 
-		this.openPage = function(path) {
-			browser.get(browser.baseUrl.concat(path))
-		}
+	openPage(path) {
+		browser.get(browser.baseUrl.concat(path))
+	}
 
-		this.clearSearchBar = function() {
-			this.searchBar.click()
-			this.searchBar.clear()
-		}
+	clearSearchBar() {
+		this.searchBar.click()
+		this.searchBar.clear()
+	}
 
-		this.performSearch = function(query) {
-			this.clearSearchBar()
-			this.searchBar.sendKeys(query)
-			this.hitEnter()
-		}
+	performSearch(query) {
+		this.clearSearchBar()
+		this.searchBar.sendKeys(query)
+		this.hitEnter()
+	}
 
-		this.hitEnter = function() {
-			browser.actions().sendKeys(protractor.Key.ENTER).perform()
-		}
+	hitEnter() {
+		browser.actions().sendKeys(protractor.Key.ENTER).perform()
 	}
 }
 
--- page-objects/homepage.js
+++ page-objects/homepage.js
@@ -1,11 +1,11 @@
 class Homepage {
 	constructor() {
-		this.topNavBar = element(by.css("ul.b-main-navigation"))
-		this.cataloqueLink = this.topNavBar.element(by.css("a[href='https://catalog.onliner.by/']"))
+	}
+	// topNavBar = element(by.css("ul.b-main-navigation"))
+	// cataloqueLink = this.topNavBar.element(by.css("a[href='https://catalog.onliner.by/']"))
 
-		this.openCatalog = function() {
-			this.cataloqueLink.click()
-		}
+	openCatalog() {
+		cataloqueLink.click()
 	}
 }
 
