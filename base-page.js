e310bc4ee843c42f3624354af16c01f211db2ced
s34rching <ivan.katsenya@gmail.com>
2019-10-10 14:19:53 +0300

    Add base-page draft

---
+++ base-page.js
@@ -0,0 +1,52 @@
+class BasePage {
+	constructor() {
+		this.searchBar = element(by.css("input.fast-search__input"))
+	}
+
+	goTo(path) {
+		browser.get(browser.baseUrl.concat(path))
+	}
+
+	clearSearchBar() {
+		this.searchBar.click()
+		this.searchBar.clear()
+	}
+
+	performSearch(query) {
+		this.clearSearchBar()
+		this.searchBar.sendKeys(query)
+		this.hitEnter()
+	}
+
+	hitEnter() {
+		browser.actions().sendKeys(protractor.Key.ENTER).perform()
+	}
+
+	// Expected Conditions
+
+	isVisible(element) {
+		return protractor.ExpectedConditions.visibilityOf(element)
+	}
+
+	isPresentInDom(element) {
+		return protractor.ExpectedConditions.presenceOf(element)
+	}
+
+	isNotVisible(element) {
+		return protractor.ExpectedConditions.invisibilityOf(element)
+	}
+
+	isNotPresentInDom(element) {
+		return protractor.ExpectedConditions.stalenessOf(element)
+	}
+
+	isClickable(element) {
+		return protractor.ExpectedConditions.elementToBeClickable(element)
+	}
+
+	hasText(element, text) {
+		return protractor.ExpectedConditions.textToBePresentInElement(element, text)
+	}
+}
+
+module.exports = BasePage
