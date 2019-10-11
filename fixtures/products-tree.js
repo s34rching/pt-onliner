ad38f86f3dbf0c55887f40087ec1e2d689214d6d
s34rching <ivan.katsenya@gmail.com>
2019-10-11 17:44:06 +0300

    Add subcategories / part #1

--- fixtures/products-tree.js
+++ fixtures/products-tree.js
@@ -1,29 +1,130 @@
 module.exports = {
 	electronics: {
-		id: 1
+		id: 1,
+		name: "",
+		subCategories: {
+			mobilePhonesAndAccessories: {},
+			televisionAnDVideo: {},
+			tabsAndEbooks: {},
+			audio: {},
+			photoAndVideo: {},
+			videoGames: {},
+			gadgets: {},
+			smarthouseAndSurveillanceVideo: {},
+			electricTransport: {},
+			telephonyAndCommunication: {},
+			musicEquipment: {},
+			opticalAppliances: {}
+		}
 	},
 	computers: {
-		id: 2
+		id: 2,
+		name: "",
+		subCategories: {
+			laptopsComputersMonitors: {},
+			components: {},
+			forPrintingAndDesign: {},
+			devicesForInputting: {},
+			dataStorage: {},
+			multimediaComponents: {},
+			networkHardware: {},
+			laptopAccessories: {},
+			chargers: {},
+			software: {}
+		}
 	},
 	houseAppliances: {
-		id: 3
+		id: 3,
+		name: "",
+		subCategories: {
+			// hobs: {},
+			// ovens: {},
+			// rangeHoods: {},
+			// dishwashers: {},
+			// fridges: {},
+			// washingMachines: {},
+			bulkyEquipment: {},
+			builtInAppliances: {},
+			cleaning: {},
+			clothesCare: {},
+			airConditioningEquipment: {},
+			cooking: {},
+			teaAndCoffeePreparation: {},
+			foodPreparation: {},
+			bodyAndHairCare: {},
+			healthEquipment: {},
+			accessoriesAndPeripheral: {}
+		}
 	},
 	constructionAndRepair: {
-		id: 4
+		id: 4,
+		name: "",
+		subCategories: {
+			tools: {},
+			buildingEquipment: {},
+			floorCoverings: {},
+			plumbing: {},
+			waterSupplySewerageVentilation: {},
+			powerSupply: {},
+			heating: {},
+			doorsAndWindows: {},
+			pneumaticTools: {},
+			machines: {},
+			buildingAndFinishingMaterials: {}
+		}
 	},
 	houseAndGarden: {
-		id: 5
+		id: 5,
+		name: "",
+		subCategories: {
+			bathroomAndToilet: {},
+			kitchen: {},
+			waterSupplyAndHeating: {},
+			furniture: {},
+			bedroom: {},
+			gardenToolsAndMachines: {},
+			dacha: {},
+			siteArrangement: {},
+			decorAndInterior: {},
+			lightningAndPowerSupply: {},
+			smartHouseAndSecurity: {},
+			clothingCare: {},
+			dishes: {},
+			celebrations: {},
+			petSupplies: {},
+			plantGrowing: {}
+		}
 	},
 	carsAndCycles: {
-		id: 6
+		id: 6,
+		name: "",
+		subCategories: {
+			sparePartsAndTechnicalFluids: {},
+			carElectronics: {},
+			repairAndWashingEquipment: {},
+			carAccessories: {},
+			autoAndMototechnics: {}
+		}
 	},
 	beautyAndSport: {
-		id: 7
+		id: 7,
+		name: "",
+		subCategories: {
+
+		}
 	},
 	childrenAndMamas: {
-		id: 8
+		id: 8,
+		name: "",
+		subCategories: {
+
+		}
 	},
 	workAndOffice: {
-		id: 9
+		id: 9,
+		name: "",
+		subCategories: {
+
+		}
 	}
 }
