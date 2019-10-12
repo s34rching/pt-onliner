bd25c9c18e21559d14908f64fb6a06744f004776
s34rching <ivan.katsenya@gmail.com>
2019-10-12 22:12:34 +0300

    Add ru names of sub categories

--- fixtures/products-tree.js
+++ fixtures/products-tree.js
@@ -1,150 +1,338 @@
 module.exports = {
 	electronics: {
 		id: 1,
-		name: "",
+		ruName: "Электроника",
 		subCategories: {
-			mobilePhonesAndAccessories: {},
-			televisionAnDVideo: {},
-			tabsAndEbooks: {},
-			audio: {},
-			photoAndVideo: {},
-			videoGames: {},
-			gadgets: {},
-			smarthouseAndSurveillanceVideo: {},
-			electricTransport: {},
-			telephonyAndCommunication: {},
-			musicEquipment: {},
-			opticalAppliances: {}
+			mobilePhonesAndAccessories: {
+				ruName: "Мобильные телефоны и аксессуары"
+			},
+			televisionAnDVideo: {
+				ruName: "Телевидение и видео"
+			},
+			tabsAndEbooks: {
+				ruName: "Планшеты, электронные книги"
+			},
+			audio: {
+				ruName: "Аудиотехника"
+			},
+			photoAndVideo: {
+				ruName: "Фото- и видеотехника"
+			},
+			videoGames: {
+				ruName: "Видеоигры"
+			},
+			gadgets: {
+				ruName: "Гаджеты"
+			},
+			smarthouseAndSurveillanceVideo: {
+				ruName: "Умный дом и видеонаблюдение"
+			},
+			electricTransport: {
+				ruName: "Электрический транспорт"
+			},
+			telephonyAndCommunication: {
+				ruName: "Телефония и связь"
+			},
+			musicEquipment: {
+				ruName: "Музыкальное оборудование"
+			},
+			opticalAppliances: {
+				ruName: "Оптические приборы"
+			}
 		}
 	},
 	computers: {
 		id: 2,
-		name: "",
+		ruName: "Компьютеры и сети",
 		subCategories: {
-			laptopsComputersMonitors: {},
-			components: {},
-			forPrintingAndDesign: {},
-			devicesForInputting: {},
-			dataStorage: {},
-			multimediaComponents: {},
-			networkHardware: {},
-			laptopAccessories: {},
-			chargers: {},
-			software: {}
+			laptopsComputersMonitors: {
+				ruName: "Ноутбуки, компьютеры, мониторы"
+			},
+			components: {
+				ruName: "Комплектующие"
+			},
+			forPrintingAndDesign: {
+				ruName: "Техника для печати и дизайна"
+			},
+			devicesForInputting: {
+				ruName: "Манипуляторы и устройства ввода"
+			},
+			dataStorage: {
+				ruName: "Хранение данных"
+			},
+			multimediaComponents: {
+				ruName: "Мультимедиа периферия"
+			},
+			networkHardware: {
+				ruName: "Сетевое оборудование"
+			},
+			laptopAccessories: {
+				ruName: "Аксессуары к ноутбукам и компьютерам"
+			},
+			chargers: {
+				ruName: "Электропитание"
+			},
+			software: {
+				ruName: "Игры и программное обеспечение"
+			}
 		}
 	},
 	houseAppliances: {
 		id: 3,
-		name: "",
+		ruName: "Бытовая техника",
 		subCategories: {
-			bulkyEquipment: {},
-			builtInAppliances: {},
-			cleaning: {},
-			clothesCare: {},
-			airConditioningEquipment: {},
-			cooking: {},
-			teaAndCoffeePreparation: {},
-			foodPreparation: {},
-			bodyAndHairCare: {},
-			healthEquipment: {},
-			accessoriesAndPeripheral: {}
+			bulkyEquipment: {
+				ruName: "Крупногабаритная техника"
+			},
+			builtInAppliances: {
+				ruName: "Встраиваемая техника"
+			},
+			cleaning: {
+				ruName: "Уборка"
+			},
+			clothesCare: {
+				ruName: "Уход за одеждой, пошив"
+			},
+			airConditioningEquipment: {
+				ruName: "Климатическая техника"
+			},
+			cooking: {
+				ruName: "Приготовление пищи"
+			},
+			teaAndCoffeePreparation: {
+				ruName: "Приготовление кофе и чая"
+			},
+			foodPreparation: {
+				ruName: "Подготовка и обработка продуктов"
+			},
+			bodyAndHairCare: {
+				ruName: "Уход за волосами и телом"
+			},
+			healthEquipment: {
+				ruName: "Техника для здоровья"
+			},
+			accessoriesAndPeripheral: {
+				ruName: "Аксессуары и сопутствующие товары"
+			}
 		}
 	},
 	constructionAndRepair: {
 		id: 4,
-		name: "",
+		ruName: "Стройка и ремонт",
 		subCategories: {
-			tools: {},
-			buildingEquipment: {},
-			floorCoverings: {},
-			plumbing: {},
-			waterSupplySewerageVentilation: {},
-			powerSupply: {},
-			heating: {},
-			doorsAndWindows: {},
-			pneumaticTools: {},
-			machines: {},
-			buildingAndFinishingMaterials: {}
+			tools: {
+				ruName: "Инструмент"
+			},
+			buildingEquipment: {
+				ruName: "Строительное оборудование"
+			},
+			floorCoverings: {
+				ruName: "Напольные покрытия"
+			},
+			plumbing: {
+				ruName: "Сантехника"
+			},
+			waterSupplySewerageVentilation: {
+				ruName: "Водоснабжение, канализация, вентиляция"
+			},
+			powerSupply: {
+				ruName: "Электроснабжение"
+			},
+			heating: {
+				ruName: "Отопление, теплоизоляция"
+			},
+			doorsAndWindows: {
+				ruName: "Двери, окна"
+			},
+			pneumaticTools: {
+				ruName: "Пневмоинструменты"
+			},
+			machines: {
+				ruName: "Станки"
+			},
+			buildingAndFinishingMaterials: {
+				ruName: "Строительные и отделочные материалы"
+			}
 		}
 	},
 	houseAndGarden: {
 		id: 5,
-		name: "",
+		ruName: "Дом и сад",
 		subCategories: {
-			bathroomAndToilet: {},
-			kitchen: {},
-			waterSupplyAndHeating: {},
-			furniture: {},
-			bedroom: {},
-			gardenToolsAndMachines: {},
-			dacha: {},
-			siteArrangement: {},
-			decorAndInterior: {},
-			lightningAndPowerSupply: {},
-			smartHouseAndSecurity: {},
-			clothingCare: {},
-			dishes: {},
-			celebrations: {},
-			petSupplies: {},
-			plantGrowing: {}
+			bathroomAndToilet: {
+				ruName: "Ванная комната и туалет"
+			},
+			kitchen: {
+				ruName: "Кухня"
+			},
+			waterSupplyAndHeating: {
+				ruName: "Водоснабжение, отопление"
+			},
+			furniture: {
+				ruName: "Мебель для дома"
+			},
+			bedroom: {
+				ruName: "Товары для спальни и домашний текстиль"
+			},
+			gardenToolsAndMachines: {
+				ruName: "Садовая техника и инструменты"
+			},
+			dacha: {
+				ruName: "Отдых на даче"
+			},
+			siteArrangement: {
+				ruName: "Обустройство участка"
+			},
+			decorAndInterior: {
+				ruName: "Декор и интерьер"
+			},
+			lightningAndPowerSupply: {
+				ruName: "Освещение и электропитание"
+			},
+			smartHouseAndSecurity: {
+				ruName: "Умный дом и безопасность"
+			},
+			clothingCare: {
+				ruName: "Уход за одеждой, стирка"
+			},
+			dishes: {
+				ruName: "Посуда и продукты питания"
+			},
+			celebrations: {
+				ruName: "Праздники"
+			},
+			petSupplies: {
+				ruName: "Зоотовары"
+			},
+			plantGrowing: {
+				ruName: "Растениеводство"
+			}
 		}
 	},
 	carsAndCycles: {
 		id: 6,
-		name: "",
+		ruName: "Авто и мото",
 		subCategories: {
-			sparePartsAndTechnicalFluids: {},
-			carElectronics: {},
-			repairAndWashingEquipment: {},
-			carAccessories: {},
-			autoAndMototechnics: {}
+			sparePartsAndTechnicalFluids: {
+				ruName: "Запчасти и технические жидкости"
+			},
+			carElectronics: {
+				ruName: "Автоэлектроника"
+			},
+			repairAndWashingEquipment: {
+				ruName: "Оборудование для ремонта и мойки"
+			},
+			carAccessories: {
+				ruName: "Аксессуары"
+			},
+			autoAndMototechnics: {
+				ruName: "Авто и мототехника"
+			}
 		}
 	},
 	beautyAndSport: {
 		id: 7,
-		name: "",
+		ruName: "Красота и спорт",
 		subCategories: {
-			bicycleAndAccessories: {},
-			rollerSkatesSkateboards: {},
-			watchBagsAccessories: {},
-			cosmeticPerfumeryAccessories: {},
-			health: {},
-			healthcareEquipment: {},
-			hobby: {},
-			tourismAndCamping: {},
-			fishingAndWaterSport: {},
-			fitnessEquipment: {},
-			gameSports: {},
-			winterSports: {}
+			bicycleAndAccessories: {
+				ruName: "Велосипеды и аксессуары"
+			},
+			rollerSkatesSkateboards: {
+				ruName: "Роликовые коньки, скейтборды, самокаты"
+			},
+			watchBagsAccessories: {
+				ruName: "Часы, сумки и прочие аксессуары"
+			},
+			cosmeticPerfumeryAccessories: {
+				ruName: "Косметика, парфюмерия, аксессуары"
+			},
+			health: {
+				ruName: "Здоровье"
+			},
+			healthcareEquipment: {
+				ruName: "Приборы по уходу за телом"
+			},
+			hobby: {
+				ruName: "Хобби"
+			},
+			tourismAndCamping: {
+				ruName: "Туризм и кемпинг"
+			},
+			fishingAndWaterSport: {
+				ruName: "Рыбалка, водный спорт"
+			},
+			fitnessEquipment: {
+				ruName: "Тренажеры, фитнес, единоборства"
+			},
+			gameSports: {
+				ruName: "Игровые виды спорта"
+			},
+			winterSports: {
+				ruName: "Зимние виды спорта"
+			}
 		}
 	},
 	childrenAndMamas: {
 		id: 8,
-		name: "",
+		ruName: "Детям и мамам",
 		subCategories: {
-			walksAndTrips: {},
-			childrenTransport: {},
-			childrenFurniture: {},
-			babySleep: {},
-			boardAndEducationalGames: {},
-			toysAndGames: {},
-			outdoorGamesAndSport: {},
-			newbornCare: {},
-			goodsForSchool: {}
+			walksAndTrips: {
+				ruName: "Прогулки и поездки"
+			},
+			childrenTransport: {
+				ruName: "Детский транспорт"
+			},
+			childrenFurniture: {
+				ruName: "Детская мебель"
+			},
+			babySleep: {
+				ruName: "Детский сон"
+			},
+			boardAndEducationalGames: {
+				ruName: "Настольные игры и развивающие игрушки"
+			},
+			toysAndGames: {
+				ruName: "Игрушки и игры"
+			},
+			outdoorGamesAndSport: {
+				ruName: "Игры на улице и спорт"
+			},
+			newbornCare: {
+				ruName: "Уход за новорожденными"
+			},
+			goodsForSchool: {
+				ruName: "Товары для школы"
+			}
 		}
 	},
 	workAndOffice: {
 		id: 9,
-		name: "",
+		ruName: "Работа и офис",
 		subCategories: {
-			officeEquipment: {},
-			officeFurniture: {},
-			tradeAndFinance: {},
-			publishingAndPrinting: {},
-			hygieneAndNutrition: {},
-			CCTV: {},
-			transportAndTransportation: {},
-			industryAndConstruction: {}
+			officeEquipment: {
+				ruName: "Офисная техника и расходные материалы"
+			},
+			officeFurniture: {
+				ruName: "Офисная мебель"
+			},
+			tradeAndFinance: {
+				ruName: "Торговля и финансы"
+			},
+			publishingAndPrinting: {
+				ruName: "Издательство и печать"
+			},
+			hygieneAndNutrition: {
+				ruName: "Гигиена и питание"
+			},
+			CCTV: {
+				ruName: "Видеонаблюдение"
+			},
+			transportAndTransportation: {
+				ruName: "Транспорт и перевозки"
+			},
+			industryAndConstruction: {
+				ruName: "Промышленность и строительство"
+			}
 		}
 	}
 }
