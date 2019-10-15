7f96ac998e3d7cecd35d2941e36a4a175c6336ee
s34rching <ivan.katsenya@gmail.com>
2019-10-15 09:30:04 +0300

    Add locators to po

--- page-objects/catalog.js
+++ page-objects/catalog.js
@@ -3,15 +3,19 @@ const BasePage = require("./base-page")
 class Catalog extends BasePage {
 	constructor() {
 		super()
-		this.classifierItem = function(itemId) {
+		this.sectionItem = function(itemId) {
 			return element(by.css(`[class="catalog-navigation-classifier__item "][data-id="${itemId}"]`))
 		}
-
-		this.productsSectionsLeftNav = element(by.css(".catalog-navigation-list__aside-list"))
+		this.categoriesLeftNav = element(by.css(".catalog-navigation-list__aside-list"))
+		this.categoryItem = element(by.css("catalog-navigation-list__aside-item"))
+		this.categoryName = this.categoryItem.element(by.css(".catalog-navigation-list__aside-title"))
+		this.subCategoriesDropDown = element(by.css(".catalog-navigation-list__dropdown-list"))
+		this.subCategoryItem = this.subCategoriesDropDown.element(by.css(".catalog-navigation-list__dropdown-item"))
+		this.subCategoryItemTitle = this.subCategoryItem.element(by.css(".catalog-navigation-list__dropdown-title"))
 	}
 
 	chooseClassifierItem(itemId) {
-		this.classifierItem(itemId).click()
+		this.sectionItem(itemId).click()
 	}
 }
 
