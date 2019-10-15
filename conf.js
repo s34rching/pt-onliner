65adc93161a59d2acf9ab9fade47efc6b41523bc
s34rching <ivan.katsenya@gmail.com>
2019-10-15 23:36:02 +0300

    Change path to specs in conf file

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ exports.config = {
 	baseUrl: "https://catalog.onliner.by",
 	framework: "jasmine",
 	seleniumAddress: "http://localhost:4444/wd/hub",
-	specs: ["/spec.js"],
+	specs: ["specs/**/*.js"],
 
 	onPrepare: () => {
 		browser.driver.manage().window().maximize()
