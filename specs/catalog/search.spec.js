8b877fb6c80ba6459ced4b1964c8a17139300f69
s34rching <ivan.katsenya@gmail.com>
2019-10-15 09:10:07 +0300

    Add draft of catalog po

---
+++ specs/catalog/search.spec.js
@@ -0,0 +1,32 @@
+const HomePage = require("../../page-objects/homepage")
+const Catalog = require("../../page-objects/catalog")
+
+describe("Onliner.by Products Search", () => {
+
+	beforeEach(() => {
+		browser.waitForAngularEnabled(false)
+	})
+
+	xit("should search product by its full product name", () => {
+		HomePage.goTo("/")
+		HomePage.performSearch("Samsung")
+	})
+
+	xit("should search products by category name", () => {
+
+	})
+
+	xit("should allow to abort search", () => {
+
+	})
+
+	xit("should NOT find non-existent product", () => {
+
+	})
+
+	it("should find product while navigating through the pages", () => {
+		HomePage.goTo("/")
+		HomePage.openCatalog()
+		Catalog.chooseClassifierItem(1)
+	})
+})
