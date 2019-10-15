9c29367e3dc38f83eac027d24d899291a0e04bbf
s34rching <ivan.katsenya@gmail.com>
2019-10-15 22:58:33 +0300

    Update conf.js
    * Set browser to start maximaized
    * Update appearance of CLI steps:

--- conf.js
+++ conf.js
@@ -2,5 +2,16 @@ exports.config = {
 	baseUrl: "https://catalog.onliner.by",
 	framework: "jasmine",
 	seleniumAddress: "http://localhost:4444/wd/hub",
-	specs: ["specs/**/*.js"]
+	specs: ["/spec.js"],
+
+	onPrepare: () => {
+		browser.driver.manage().window().maximize()
+	},
+	jasmineNodeOpts: {
+		showColors: true,
+		displaySpecDuration: true,
+		print: () => {},
+		defaultTimeoutInterval: 50000
+	}
 }
+
