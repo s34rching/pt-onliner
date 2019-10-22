c6cbab4f8e2a9b9758233ee31e3e3896579758be
s34rching <ivan.katsenya@gmail.com>
2019-10-22 11:51:50 +0300

    Add descriptional wrapper to test

--- specs/catalog/navigation.spec.js
+++ specs/catalog/navigation.spec.js
@@ -8,23 +8,26 @@ describe("Onliner.by - Catalog / Navigation", () => {
 		browser.waitForAngularEnabled(false)
 	})
 
-	it("should find products while navigating through the pages", () => {
+	describe("When user uses pages navigation through the 'Catalog'", () => {
+		it("then they should be able to products on subcategory pages", () => {
 
-		const randomClassifierItem = entities.getRandomClassifierItem()
-		const randomCategoryItem = entities.getRandomCategory(randomClassifierItem)
-		const randomSubcategoryItem = entities.getRandomSubcategory(randomCategoryItem)
+			const randomClassifierItem = entities.getRandomClassifierItem()
+			const randomCategoryItem = entities.getRandomCategory(randomClassifierItem)
+			const randomSubcategoryItem = entities.getRandomSubcategory(randomCategoryItem)
 
-		HomePage.goTo("/")
-			.then(() => { HomePage.openCatalog() })
-			.then(() => { Catalog.chooseClassifierItem(randomClassifierItem.id) })
-			.then(() => { Catalog.focusCategoryItem(randomCategoryItem.ruName) })
-			.then(() => { Catalog.openSubcategory(randomSubcategoryItem.ruName) })
-			.then(() => {
-				expect(element(by.css("h1.schema-header__title")).getText()).toBe(randomSubcategoryItem.ruName)
-				expect(element(by.css("#schema-products")).isDisplayed()).toBe(true)
-				expect(element(by.css("#schema-filter")).isDisplayed()).toBe(true)
-			})
+			HomePage.goTo("/")
+				.then(() => { HomePage.openCatalog() })
+				.then(() => { Catalog.chooseClassifierItem(randomClassifierItem.id) })
+				.then(() => { Catalog.focusCategoryItem(randomCategoryItem.ruName) })
+				.then(() => { Catalog.openSubcategory(randomSubcategoryItem.ruName) })
+				.then(() => {
+					expect(element(by.css("h1.schema-header__title")).getText()).toBe(randomSubcategoryItem.ruName)
+					expect(element(by.css("#schema-products")).isDisplayed()).toBe(true)
+					expect(element(by.css("#schema-filter")).isDisplayed()).toBe(true)
+				})
+		})
 	})
+
 })
 
 // TODO: search sequence object
