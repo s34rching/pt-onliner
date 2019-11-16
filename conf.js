91e510d8ce11ed76f9ee107e9a08065091edf5aa
Ivan Katsenya <ivan.katsenya@freshly.com>
2019-11-16 22:40:15 +0300

    Setup new CLI spec-reporter

--- conf.js
+++ conf.js
@@ -1,11 +1,25 @@
+let SpecReporter = require("jasmine-spec-reporter").SpecReporter
+
 exports.config = {
 	baseUrl: "https://catalog.onliner.by",
 	framework: "jasmine",
 	seleniumAddress: "http://localhost:4444/wd/hub",
 	specs: ["specs/**/*.js"],
+	jasmineNodeOpts: {
+		print: function() {}
+	},
 
 	onPrepare: () => {
 		browser.driver.manage().window().maximize()
+		jasmine.getEnv().addReporter(new SpecReporter({
+			spec:
+				{
+					displayFailuresSummary: true,
+					displayFailuredSpec: true,
+					displaySuiteNumber: true,
+					displaySpecDuration: true
+				}
+		}))
 	}
 }
 
