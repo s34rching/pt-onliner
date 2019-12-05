67bba6e588db1be878235e0ebd4da86d67d4799a
Ivan Katsenya <ivan.katsenya@freshly.com>
2019-12-05 22:49:25 +0300

    Add elements to SearchIframe constructor

--- page-objects/search-iframe.js
+++ page-objects/search-iframe.js
@@ -4,6 +4,19 @@ class SearchIframe extends BasePage {
 	constructor() {
 		super()
 		this.searchIframe = element(by.className("modal-iframe"))
+		this.productTitle = function(productTitle) {
+			return element(by.cssContainingText(".product__title-link", productTitle))
+		}
+		this.resultItemProduct = function(productTitle) {
+			return this.productTitle(productTitle).element(by.xpath("ancestor::div[@class='result__item_product']"))
+		}
+		this.productPrice = function(productTitle) {
+			return this.resultItemProduct(productTitle).element(by.css(".product-price"))
+		}
+		this.resultItemCategory = function(categoryTitle) {
+			return element(by.css(".result__item_category"))
+				.element(by.cssContainingText(".category__title", categoryTitle))
+		}
 	}
 
 	switchToSearchIframe() {
