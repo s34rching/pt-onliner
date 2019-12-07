a780fd4f42bddcc1579764452d8827b94989bf44
Ivan Katsenya <ivan.katsenya@freshly.com>
2019-12-07 13:22:53 +0300

    Add 'products' fixture

---
+++ fixtures/products.js
@@ -0,0 +1,143 @@
+module.exports = {
+	available:
+		[
+			{
+				query: "Смартфон Apple iPhone 11 64GB (черный)",
+				catalogTitle: "Смартфон Apple iPhone 11 64GB (черный)",
+				relativeUrl: "/mobile/apple/iphone1164b",
+				status: "active",
+				label: null,
+				price: 1890,
+				accuracy: 200
+			},
+			{
+				query: "AMD Ryzen 5 3600",
+				catalogTitle: "Процессор AMD Ryzen 5 3600",
+				relativeUrl: "/cpu/amd/rzn53600",
+				status: "active",
+				label: null,
+				price: 415,
+				accuracy: 100
+			},
+			{
+				query: "Холодильник side by side Samsung RS552NRUASL",
+				catalogTitle: "Холодильник side by side Samsung RS552NRUASL",
+				relativeUrl: "/refrigerator/samsung/rs552nruasl",
+				status: "active",
+				label: null,
+				price: 3029,
+				accuracy: 200
+			},
+			{
+				query: "Apple iPad Air 2019 64GB MUUJ2 (серый космос)",
+				catalogTitle: "Планшет Apple iPad Air 2019 64GB MUUJ2 (серый космос)",
+				relativeUrl: "/ssd/samsung/mz76e250bw",
+				status: "active",
+				label: null,
+				price: 1250,
+				accuracy: 100
+			},
+			{
+				query: "SSD Samsung 860 Evo 250GB MZ-76E250",
+				catalogTitle: "SSD Samsung 860 Evo 250GB MZ-76E250",
+				relativeUrl: "/tabletpc/apple/muuj2",
+				status: "active",
+				label: null,
+				price: 105,
+				accuracy: 30
+			}],
+	outOfStock:
+		[
+			{
+				query: "Велосипед Cube Stereo 150 HPA Race 27.5+ (2017)",
+				catalogTitle: "Велосипед Cube Stereo 150 HPA Race 27.5+ (2017)",
+				relativeUrl: "/bike/cube/150hparace275pl",
+				status: "out-of-stock",
+				label: "Нет в наличии",
+				price: null,
+				accuracy: null
+			},
+			{
+				query: "DeWalt D25404K",
+				catalogTitle: "Перфоратор DeWalt D25404K",
+				relativeUrl: "/rotaryhammers/dewalt/d25404k",
+				status: "out-of-stock",
+				label: "Нет в наличии",
+				price: null,
+				accuracy: null
+			},
+			{
+				query: "Душевая кабина Erlit ER NYC100W-2 100x105",
+				catalogTitle: "Душевая кабина Erlit ER NYC100W-2 100x105",
+				relativeUrl: "/showerbox/erlit/ernyc100w2",
+				status: "out-of-stock",
+				label: "Нет в наличии",
+				price: null,
+				accuracy: null
+			},
+			{
+				query: "Thermaltake Versa H26 Tempered Glass Edition CA-1J5-00M1WN-00",
+				catalogTitle: "Корпус Thermaltake Versa H26 Tempered Glass Edition CA-1J5-00M1WN-00",
+				relativeUrl: "/chassis/thermaltake/ca1j500m1wn00",
+				status: "out-of-stock",
+				label: "Нет в наличии",
+				price: null,
+				accuracy: null
+			},
+			{
+				query: "МФУ Kyocera Mita ECOSYS M6026cidn",
+				catalogTitle: "МФУ Kyocera Mita ECOSYS M6026cidn",
+				relativeUrl: "/printers/kyoceramita/ecosysm6026cidn",
+				status: "out-of-stock",
+				label: "Нет в наличии",
+				price: null,
+				accuracy: null
+			}],
+	offSale:
+		[
+			{
+				query: "Intel Core 2 Duo E7500",
+				catalogTitle: "Процессор Intel Core 2 Duo E7500",
+				relativeUrl: "/cpu/intel/intele7500",
+				status: "off-sale",
+				label: "Снят с продажи",
+				price: null,
+				accuracy: null
+			},
+			{
+				query: "Умные часы Apple Watch Series 1 42mm Rose Gold with Pink Sand Sport Band [MQ112]",
+				catalogTitle: "Умные часы Apple Watch Series 1 42mm Rose Gold with Pink Sand Sport Band [MQ112]",
+				relativeUrl: "/smartwatch/apple/applemq112",
+				status: "off-sale",
+				label: "Снят с продажи",
+				price: null,
+				accuracy: null
+			},
+			{
+				query: "Siemens ET875LMP1D",
+				catalogTitle: "Варочная панель Siemens ET875LMP1D",
+				relativeUrl: "/hob_cooker/siemens/et875lmp1d",
+				status: "off-sale",
+				label: "Снят с продажи",
+				price: null,
+				accuracy: null
+			},
+			{
+				query: "Эспрессо кофемашина Philips HD8847/09",
+				catalogTitle: "Эспрессо кофемашина Philips HD8847/09",
+				relativeUrl: "/coffee/philips/hd884709",
+				status: "off-sale",
+				label: "Снят с продажи",
+				price: null,
+				accuracy: null
+			},
+			{
+				query: "Велосипед GT Aggressor 1.0 Mech (2014)",
+				catalogTitle: "Велосипед GT Aggressor 1.0 Mech (2014)",
+				relativeUrl: "/bike/gt/aggressor10mech",
+				status: "off-sale",
+				label: "Снят с продажи",
+				price: null,
+				accuracy: null
+			}]
+}
