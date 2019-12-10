c95ebf75db661adb6f01a9b9aed1d2b530bb9245
Ivan Katsenya <ivan.katsenya@freshly.com>
2019-12-10 22:32:23 +0300

    Remove buggy locators

--- page-objects/products-list.js
+++ page-objects/products-list.js
@@ -3,21 +3,9 @@ const BasePage = require("./base-page")
 class ProductsList extends BasePage {
 	constructor() {
 		super()
-		this.subcategoryTitle = element(by.css("h1.schema-header__title"))
 		this.orderOptionsContainer = element(by.id("schema-order"))
 		this.orderDropdown = this.orderOptionsContainer.element(by.css("a.schema-order__link"))
 		this.orderDropdownActiveOrderOption = this.orderDropdown.element(by.css("span.schema-order__text"))
-		this.orderDropdownOptionsList = this.orderDropdown.element(by.css("div.schema-order__list"))
-		this.orderDropdownOption = function(option) {
-			return this.orderDropdownOptionsList.element(by.cssContainingText("span", option)) }
-		this.listsSwitcher = function(list) {
-			return element(by.cssContainingText("span.schema-filter-control__switcher-inner", list))
-		}
-		this.createUsedAdButton = element(by.css("a[href='/used/create']"))
-		this.productItems = element.all(by.css("schema-product__group"))
-		this.firstProductItem = element.all(by.css("schema-product__group")).first()
-		this.productTitle = function(title) {
-			element(by.css("schema-product__group")).element(by.cssContainingText("span", title))}
 	}
 
 	waitForOrderDefaultOptionIsDisplayed() {
--- specs/catalog-products-list.spec.js
+++ specs/catalog-products-list.spec.js
@@ -6,18 +6,35 @@ describe("Onliner.by - Catalog / Products List", () => {
 		browser.waitForAngularEnabled(false)
 	})
 
-	it("default sort order is set as 'Popular'", () => {
+	xit("default sort order is set as 'Popular'", () => {
 		ProductsList.goTo("/cpu")
 		ProductsList.waitForOrderDefaultOptionIsDisplayed()
 		ProductsList.orderDropdownActiveOrderOption.getText().then(text => {
 			expect(text).toBe("популярные")
 		})
 	})
-	xit("user should be able to order products", () => {})
-	xit("user should be able to filter products", () => {})
-	xit("user should be able to reset filters", () => {})
-	xit("user should be able to add products for comparison", () => {})
-	xit("user should be able to open offers list page", () => {})
-	xit("user should be able to observe user's ads", () => {})
-	xit("user should be able to create ad", () => {})
+	xit("user should be able to order products", () => {
+
+	})
+	xit("user should be able to filter products", () => {
+
+	})
+	xit("user should be able to reset filters", () => {
+
+	})
+	xit("user should be able to add products for comparison", () => {
+
+	})
+	xit("user should be able to open offers list page", () => {
+
+	})
+	xit("user should be able to observe user's ads", () => {
+
+	})
+	xit("user should be able to create ad", () => {
+		ProductsList.goTo("/cpu")
+		ProductsList.switchToUsedAdsList("Объявления")
+		ProductsList.initiateCreationOfUsedAd()
+		browser.sleep(5000)
+	})
 })
