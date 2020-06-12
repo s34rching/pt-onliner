4ab1052d47f34d98717970848b4522111427f0b1
Ivan Katsenya <ivan.katsenya@freshly.com>
2020-06-12 13:07:52 +0300

    Informers: update service method

--- service/location-services.js
+++ service/location-services.js
@@ -1,7 +1,7 @@
 module.exports = {
 	getLocationsAmount: showMapMessage => {
 		let textValue
-		const matches = { 2: "два", 3: "три", 4: "четыре", 5: "пять", 6: "шесть", 7: "семь", 8: "восемь", 9: "девять" }
+		const matches = { 2: "двух", 3: "трех", 4: "четырех", 5: "пяти", 6: "шести", 7: "семи", 8: "восьми", 9: "девяти" }
 
 		if (isMoreThanNineLocations(showMapMessage)) {
 			textValue = isMoreThanNineLocations(showMapMessage)[0]
@@ -31,12 +31,12 @@ module.exports = {
 
 const isOnlyLocation = message => !message.match(/^\u0412\s/)
 const isMoreThanNineLocations = message => message.match(/\d+/)
-const getLiteralLocationsAmount = message => message.match(/(?<=^\u0412\s)\w+(?=\s)/)
+const getLiteralLocationsAmount = message => message.match(/(?<=^\u0412\s).+(?=\s)/)
 const hasPluralEnding = amount => {
 	const withSingularEnding = [ 2, 3, 4 ]
 
 	return !((withSingularEnding.includes(amount)) || ([ 1, 2 ].includes(amount % 10) && ![ 11, 12 ].includes(amount)))
 }
 const getKeyByValue = (object, value) => {
-	return Object.keys(object).find(key => object[key] === value)
+	return Object.keys(object).find(key => object[key] === value.split(" ")[0])
 }
