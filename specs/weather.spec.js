0d003ad7f8a0fa1b938c9fa39d40f42dbbc37b72
s34rching <ivan.katsenya@gmail.com>
2020-07-06 16:57:38 +0300

    Add weather data formatter

--- specs/weather.spec.js
+++ specs/weather.spec.js
@@ -4,16 +4,19 @@ const WeatherForecastPage = require('../page-objects/weather-forecast-page');
 const { cities } = require('../fixtures/cities');
 const api = require('../helpers/onliner-api');
 const { composeUrl } = require('../helpers/url-composer');
+const { getFormattedTemperature } = require('../service/weather-services');
 
 describe('Onliner.by - Top Navigation / Informers - Weather Forecast', () => {
   const userCity = _.sample(_.without(cities, { name: 'Минск' }));
-  let forecast; let
-    userCityForecast;
+  let forecast;
+  let userCityForecast;
+  let currentTemperature;
 
   beforeAll((done) => {
     api.getWeather()
       .then((res) => {
         forecast = JSON.parse(res);
+        currentTemperature = forecast.now.temperature;
         return api.getWeather(userCity.id);
       })
       .then((res) => {
@@ -28,7 +31,7 @@ describe('Onliner.by - Top Navigation / Informers - Weather Forecast', () => {
 
   it("User should be able to see current temperature in user's default city on homepage", () => {
     HomePage.constructor.goTo('/');
-    expect(HomePage.currentTemperature.getText()).toBe(`${forecast.now.temperature}°`);
+    expect(HomePage.currentTemperature.getText()).toBe(getFormattedTemperature(currentTemperature));
   });
 
   it('User should be able open weather forecast page', () => {
