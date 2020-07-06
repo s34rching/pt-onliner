b7970f75ef62f667b404de2009ee14c542f94445
s34rching <ivan.katsenya@gmail.com>
2020-07-06 13:27:35 +0300

    Remove service file

--- service/constants.js
+++ service/constants.js
@@ -1,6 +1,3 @@
 module.exports = {
-  PRODUCTS_PER_PAGE: 30,
-  EXPECTED_PRICE_CHANGE: 0.25,
-  CURRENCIES: ['BYN', 'RUB', 'USD', 'EUR'],
-  CONVERSION_DIRECTIONS: ['usd-to-byn', 'eur-to-byn', 'rub-to-byn', 'eur-to-usd', 'eur-to-rub', 'usd-to-rub'],
+
 };
