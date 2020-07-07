44a2b98ab0f17170c5da95e56351f01e867ce472
s34rching <ivan.katsenya@gmail.com>
2020-07-07 16:03:33 +0300

    Catalog: update pattern

--- page-objects/catalog.js
+++ page-objects/catalog.js
@@ -7,7 +7,7 @@ class Catalog extends BasePage {
   constructor() {
     super();
     this.sectionItem = (itemId) => $(`[class="catalog-navigation-classifier__item "][data-id="${itemId}"]`);
-    this.categoryItem = (categoryTitle) => element(by.cssContainingText('.catalog-navigation-list__aside-title', categoryTitle.match(/^\S+/)));
+    this.categoryItem = (categoryTitle) => element(by.cssContainingText('.catalog-navigation-list__aside-title', categoryTitle.match(/^\S+/)[0]));
     this.subCategoryItem = (subcategoryItem) => $(`a[href$="${subcategoryItem.path}"][class="catalog-navigation-list__dropdown-item"]`);
     this.catalogBar = element(by.className('catalog-bar'));
     this.tile = (tileTitle) => element.all(by.xpath(`//div[contains(@class, tiles__item) and descendant::span[contains(., '${tileTitle}')]]`)).last();
